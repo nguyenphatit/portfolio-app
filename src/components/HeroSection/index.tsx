c8e4755fbb1606fcb864aca64559882e924de115
Phat <nguyenphatit.nl@gmail.com>
2022-05-26 20:40:04 +0700

    developing

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,17 +1,26 @@
 import styled from "styled-components";
-import ListAnimation from "components/ListAnimation";
 
 const Wrapper = styled.section`
     background-color: #132b79;
     background-image: url("images/bg-hero.jpg");
     background-repeat: no-repeat;
     background-size: cover;
+`;
+
+const Content = styled.div`
+    background-color: rgba(0, 0, 0, 0.5);
+    backdrop-filter: saturate(180%) blur(20px);
 `
 
 const HeroSection = () => {
     return (
         <Wrapper className="h-screen flex justify-center items-center origin-center w-full">
-            <h1>HELLO</h1>
+            <Content className="flex flex-col justify-center items-center p-6 rounded-lg">
+                <div className="font-heading text-7xl text-warning uppercase">Welcome to</div>
+                <div className="font-heading text-7xl text-warning uppercase">
+                    the Tour De Force
+                </div>
+            </Content>
         </Wrapper>
     );
 };
