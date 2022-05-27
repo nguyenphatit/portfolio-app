cfde1c40e15c5a7cc69aaa0b7a6161efe67c212c
Phat <nguyenphatit.nl@gmail.com>
2022-05-27 21:02:30 +0700

    developing

--- src/components/Home/AboutSection/index.tsx
+++ src/components/Home/AboutSection/index.tsx
@@ -1,45 +1,77 @@
 import styled from "styled-components";
 
 const TextCard = styled.div`
-    background-color: #EC5E4F;
+    background-color: #ec5e4f;
     background-image: url("images/jhin.jpg");
     background-repeat: no-repeat;
     background-size: cover;
-`
+    border-radius: 30px;
+`;
 
 const AboutSection = () => {
     return (
-        <section className="bg-[#F5F5F7]">
-            <div className="container mx-auto py-10">
-                <div className="w-full">
-                    <h1 className="text-6xl font-bold text-error py-10">
-                        About.
-                    </h1>
-                    <TextCard className="text-2xl text-justify w-full max-h-128 p-6 text-light rounded-xl">
-                        Hey, I am Okiki Ojo (pronounced O-key-key O-jo), I’m a
-                        student currently studying Software Engineering at
-                        Ontario Tech University (formerly University of Ontario
-                        Institute of Technology, UOIT). From a very young age,
-                        I've always been interested in tech, and science.
-                        Initially, I started web development out of curiosity,
-                        eventually it became a hobby, and after many hobby
-                        projects (e.g. Open Source projects; read more in
-                        featured projects ), I thought it would be cool to offer
-                        my skills to clients who want awesome web experiences,
-                        thus here we are. I have a passion for design and
-                        development, I like to create things that don’t just
-                        look good, but that also function on multiple levels. I
-                        am always eager to take on a challenge, and aim to
-                        exceed in all tasks I take on. I develop websites that
-                        are functional, efficient and that act as an excellent
-                        representation of a person or business on the digital
-                        landscape. I have 5+ years of experience designing
-                        sites, I would love to bring your digital vision to
-                        reality.
-                    </TextCard>
+        <>
+            <section className="bg-[#F5F5F7]">
+                <div className="container mx-auto py-20">
+                    <div className="w-full">
+                        <h1 className="text-6xl font-bold text-success py-10">
+                            About.
+                        </h1>
+                        <TextCard className="w-full h-[70vh] p-10 relative">
+                            <h1 className="font-bold text-success text-3xl">
+                                Hey, I am Phat Nguyen
+                            </h1>
+                            <p className="font-bold text-light text-lg w-full sm:w-3/4 leading-relaxed">
+                                I am a software developer currently working at
+                                TMA Solutions in Ho Chi Minh City. Since high
+                                school* , I started web development out of
+                                curiosity, eventually it became a hobby, and
+                                after many hobby projects, I thought it would be
+                                great to offer my skills to clients. Customers
+                                want a great web experience, so here I am. I
+                                have a passion for design and development, I
+                                want to create things that not only look good
+                                but work on multiple levels
+                            </p>
+                            <div className="absolute text-right right-10 bottom-10 italic text-light">
+                                *Designing a simple website - Pages 162 - 164 in
+                                the 10th grade Informatics book{" "}
+                                <a
+                                    rel="noreferrer"
+                                    target="_blank"
+                                    href="https://drive.google.com/file/d/141jjnLSjcxD3L-2AxjZHL5YZwcaxUSGp/view"
+                                >
+                                    <span className="material-symbols-outlined text-sm">
+                                        open_in_new
+                                    </span>
+                                </a>
+                            </div>
+                        </TextCard>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+            <section className="bg-[#F5F5F7]">
+                <div className="container mx-auto py-20">
+                    <div className="w-full">
+                        <h1 className="text-6xl font-bold text-dark py-10">
+                            Experiences.
+                        </h1>
+                        <div className="font-bold text-dark text-lg w-full sm:w-3/4 leading-relaxed pl-6">
+                            <ol className="list-disc">
+                                <li>3 years React.JS development</li>
+                                <li>2 years Node.JS development</li>
+                                <li>1+ years Angular development</li>
+                                <li>
+                                    Experience working with using remote data
+                                    via REST APIs, JSON, and GraphQL as well as
+                                    integrating with third-party libraries
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     );
 };
 
